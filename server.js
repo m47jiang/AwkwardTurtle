22cf281756a6bf848621eb3d0c21b87fc0289315
Elwazirali <elwazirali@gmail.com>
2016-06-04 19:40:23 -0400

    adding bootstrap

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res){	
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('chatRoom', function(req, res){	
+  res.sendFile(__dirname + '/chatRoom.js');
 });
 
 io.on('connection', function(socket){
