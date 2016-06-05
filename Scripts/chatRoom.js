1560ac4008d5bae61620d217cce922a217e394b7
GitSaad <saadsrahimi@gmail.com>
2016-06-05 01:25:42 -0400

    Adding WebCam feature

--- Scripts/chatRoom.js
+++ Scripts/chatRoom.js
@@ -1,1 +1,14 @@
 alert("Front-End Script Check");
+var socket = io();
+var color = prompt("Color");
+var userName = '<span ' + 'style="color:' + color +';"'+ '>'+ prompt("Your Nickname:") + ':' +'</span>';
+socket.emit('add user', userName);
+$('form').submit(function(){
+	socket.emit('chat message', $('#m').val(), userName);
+	$('#m').val('');
+	return false;
+});
+socket.on('chat message', function(msg, username){
+	$.parseHTML(userName);
+	$('#messages').append($('<li>').html(msg).prepend(username+' '));
+});
