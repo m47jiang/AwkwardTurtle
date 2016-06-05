9bf6b1e4da9a7fd35f00a5635f72cf27f9b9ef37
GitSaad <saadsrahimi@gmail.com>
2016-06-04 20:07:26 -0400

    Resolving chatRoom.js conflict

--- server.js
+++ server.js
@@ -7,13 +7,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-<<<<<<< HEAD
-app.get('chatRoom', function(req, res){	
-  res.sendFile(__dirname + '/chatRoom.js');
-});
-=======
 app.use(express.static('Scripts'));
->>>>>>> a027063701e419321d6901379692f1f7fe7e68b3
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
