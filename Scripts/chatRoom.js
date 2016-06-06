d885e36d6dea3cec5468ecec9695674b808d5e86
Elwazirali <elwazirali@gmail.com>
2016-06-05 22:54:30 -0400

    latest

--- Scripts/chatRoom.js
+++ Scripts/chatRoom.js
@@ -115,7 +115,7 @@ $('form').submit(function(){
 $('#m').on('input', function() {
     if(executedTimer)
     {
-        messageData[0].typingStart =  new Date().getTime().toString();
+        messageData[0].typingStart =  new Date().getTime();
         executedTimer = false;
     }
     else{
@@ -137,31 +137,74 @@ socket.on('chat message', function(msg, username) {
         }
     }
 
-    messageData2.Inputs.input1.Values[0][0] = msg.split(" ").length;
+    messageData2.Inputs.input1.Values[0][0] = (msg.split(" ").length).toString();
 
     if(msg.includes("."))
     {
         messageData[0].period++;
-        messageData2.Inputs.input1.Values[0][7]++;
+        (messageData2.Inputs.input1.Values[0][7]++).toString();
 
     }
     if(msg.includes("?"))
     {
         messageData[0].question++;
-        messageData2.Inputs.input1.Values[0][4]++;
+        (messageData2.Inputs.input1.Values[0][4]++).toString();
     }
     if(msg.includes("!"))
     {
         messageData[0].exclimation++;
-        messageData2.Inputs.input1.Values[0][5]++;
+        (messageData2.Inputs.input1.Values[0][5]++).toString();
     }
-    messageData2.Inputs.input1.Values[0][9] = messagesSent;
+    messageData2.Inputs.input1.Values[0][9] = messagesSent.toString();
     messageData[0].timesent =  new Date().getTime().toString();
-    messageData2.Inputs.input1.Values[0][3] =  messageData[0].typingStart;
-    messageData2.Inputs.input1.Values[0][2] =  messageData[0].timesent - messageData[0].typingStart;
+    messageData2.Inputs.input1.Values[0][3] =  (new Date().getTime()-messageData[0].typingStart).toString();
+    messageData2.Inputs.input1.Values[0][2] =  (messageData[0].timesent - messageData[0].typingStart).toString();
 
     $('#messages').append($('<h3>').html(msg).prepend(username + ' ' + ' '));
+
     $("html, body").animate({scrollTop: $(document).height()}, 10);
     executedTimer = true;
     messagesSent++;
+
+    /*$.ajax({
+        type: "POST",
+        url: 'https://ussouthcentral.services.azureml.net/workspaces/fd241c11b0734e87b79a7df5a11b6639/services/909a3822fcf440d0b026b0b333ea21ca/execute?api-version=2.0&details=true',
+        /*beforeSend: function (request)
+        {
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.setRequestHeader('Authorization', 'Bearer WMy+cO/0vNMJKOVXui9OYnhyysmPt8EG1SXF3rvLYt6eZUvxVX+i+O/3h0EIL9YLGEvgFP8uCJYxdECCo/VRIg==');
+        },
+        headers: {
+            'Content-Type' :  'application/json;charset=utf-8',
+            'Authorization' : 'Bearer WMy+cO/0vNMJKOVXui9OYnhyysmPt8EG1SXF3rvLYt6eZUvxVX+i+O/3h0EIL9YLGEvgFP8uCJYxdECCo/VRIg=='
+        },
+        data: messageData2,//JSON.stringify(messageData2),
+        //dataType: 'application/json',
+        //processData: false,
+        success: function(data) {
+            //$('#results').append('The result =' + StringifyPretty(msg));
+            alert('sent');
+        },
+        error: function(msg){
+            alert('error: ' + msg.status + ' ' + msg.responseText);
+        }
+    });
+*/
+    console.log(JSON.stringify(messageData2));
+    var url= "https://ussouthcentral.services.azureml.net/workspaces/fd241c11b0734e87b79a7df5a11b6639/services/909a3822fcf440d0b026b0b333ea21ca/execute?api-version=2.0&details=true";
+
+    $.ajax({
+        url: url,
+        method: "POST",
+        contentType: 'application/json',
+        processData: false,
+        crossDomain: true,
+        headers: {
+            "Authorization": "Bearer WMy+cO/0vNMJKOVXui9OYnhyysmPt8EG1SXF3rvLYt6eZUvxVX+i+O/3h0EIL9YLGEvgFP8uCJYxdECCo/VRIg=="
+        },
+        data: JSON.stringify(messageData2),
+        done: function(res){
+            console.log(res);
+        }
+    });
 });
